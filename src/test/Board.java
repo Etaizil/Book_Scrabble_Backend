1ac5b1a9bcea15bc015d0383c58c3081872320ac
Etai zilberman <etai444@gmail.com>
2024-03-13 11:00:44 +0200

    Added project files, started implementing classes

---
+++ src/test/Board.java
@@ -0,0 +1,9 @@
+/*
+@created 12/03/2024 - 21:29
+@project PTM1 Project
+@author Zilberman Etai
+*/
+
+package test;
+
+public class Board {}
---
+++ src/test/Word.java
@@ -0,0 +1,9 @@
+/*
+@created 12/03/2024 - 21:30
+@project PTM1 Project
+@author Zilberman Etai
+*/
+
+package test;
+
+public class Word {}
